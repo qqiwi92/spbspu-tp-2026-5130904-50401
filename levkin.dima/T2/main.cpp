9ec362772cf778392ca7332fe4fef113a7fd34a0
Dmitry Levkin <mit.levkin@vk.com>
2026-06-18 18:43:15 +0300

    Refactor input parsing and update output format for p_t

--- levkin.dima/T2/main.cpp
+++ levkin.dima/T2/main.cpp
@@ -7,10 +7,64 @@ namespace levkin {
 struct p_t {
   int x, y;
 };
+
+struct delimeter_t {
+  char expected;
+};
+
 std::ostream& operator<<(std::ostream& os, p_t p);
 std::istream& operator>>(std::istream& is, p_t& p);
-
+std::istream& operator>>(std::istream& is, delimeter_t del);
 bool operator<(p_t lhs, p_t rhs);
+}
+
+std::istream& check(std::istream& is, char ch)
+{
+  char c = 0;
+  if (is >> c && c != ch) {
+    is.setstate(std::ios_base::failbit);
+  }
+  return is;
+}
+
+std::istream& levkin::operator>>(std::istream& is, delimeter_t del)
+{
+  return check(is, del.expected);
+}
+
+std::ostream& levkin::operator<<(std::ostream& os, levkin::p_t p)
+{
+  std::ostream::sentry s(os);
+  if (!s) {
+    return os;
+  }
+  return os << '(' << p.x << "; " << p.y << ')';
+}
+
+std::istream& levkin::operator>>(std::istream& is, p_t& p)
+{
+  std::istream::sentry s(is);
+  if (!s) {
+    return is;
+  }
+
+  int x = 0, y = 0;
+  using del_t = delimeter_t;
+
+  is >> del_t{'('} >> x >> del_t{';'} >> y >> del_t{')'};
+
+  if (is) {
+    p = p_t{x, y};
+  }
+  return is;
+}
+
+bool levkin::operator<(p_t lhs, p_t rhs)
+{
+  if (lhs.x != rhs.x) {
+    return lhs.x < rhs.x;
+  }
+  return lhs.y < rhs.y;
 }
 
 int main()
@@ -20,64 +74,10 @@ int main()
 
   using iit_t = std::istream_iterator< T >;
   using oit_t = std::ostream_iterator< T >;
+
   std::copy(iit_t{std::cin}, iit_t{}, std::back_inserter(data));
 
   std::sort(data.begin(), data.end());
 
   std::copy(data.begin(), data.end(), oit_t{std::cout, "\n"});
-}
-
-std::ostream& levkin::operator<<(std::ostream& os, levkin::p_t p)
-{
-  return os << '(' << p.x << ',' << ' ' << p.y << ')';
-}
-
-std::istream& levkin::operator>>(std::istream& is, p_t& p)
-{
-  if (!is) {
-    return is;
-  }
-  char c = 0;
-  is >> c;
-  if (c != '(') {
-    is.setstate(std::ios_base::failbit);
-    return is;
-  }
-  int x = 0;
-  is >> x;
-  if (!is) {
-    return is;
-  }
-  is >> c;
-  if (c != ',') {
-     is.setstate(std::ios_base::failbit);
-     return is;
-   }
-  int y = 0;
-  is >> y;
-  if (!is) {
-    return is;
-  }
-
-  is >> c;
-  if (c != ')') {
-    is.setstate(std::ios_base::failbit);
-    return is;
-  }
-  if (is) {
-    p = p_t{x, y};
-  }
-  return is;
-}
-
-bool levkin::operator<(p_t lhs, p_t rhs)
-{
-  if (lhs.x < rhs.x) {
-    return true;
-  } else if (lhs.x == rhs.x) {
-    if (lhs.y < rhs.y) {
-      return true;
-    }
-  }
-  return false;
 }
