85ce98144481b691d73077a657fa833f902b152c
Dmitry Levkin <mit.levkin@vk.com>
2026-06-18 23:52:29 +0300

    Refactor I/O operations and fix logic in DataStruct

--- levkin.dima/T2/main.cpp
+++ levkin.dima/T2/main.cpp
@@ -3,7 +3,7 @@
 #include <string>
 #include <algorithm>
 #include <iterator>
-#include <iomanip>
+#include <limits>
 
 namespace levkin {
 using Ratio = std::pair< long long, unsigned long long >;
@@ -17,45 +17,32 @@ struct DataStruct {
 struct DelimiterIO {
   char exp;
 };
-
 struct LabelIO {
   std::string exp;
 };
-
 struct UllLitIO {
   unsigned long long& ref;
 };
-
 struct RatLspIO {
   Ratio& ref;
 };
-
 struct StringIO {
   std::string& ref;
 };
 
 std::istream& operator>>(std::istream& in, DelimiterIO&& dest)
 {
-  std::istream::sentry sentry(in);
-  if (!sentry)
-    return in;
-  char c = ' ';
-  in >> c;
-  if (in && c != dest.exp) {
+  char c;
+  if (in >> c && c != dest.exp)
     in.setstate(std::ios::failbit);
-  }
   return in;
 }
 
 std::istream& operator>>(std::istream& in, LabelIO&& dest)
 {
-  std::istream::sentry sentry(in);
-  if (!sentry)
-    return in;
   for (char exp_c : dest.exp) {
-    char c = ' ';
-    in >> c;
-    if (in && std::tolower(c) != std::tolower(exp_c)) {
+    char c;
+    if (in >> c && std::tolower(c) != std::tolower(exp_c)) {
       in.setstate(std::ios::failbit);
       break;
     }
@@ -65,34 +52,24 @@ std::istream& operator>>(std::istream& in, LabelIO&& dest)
 
 std::istream& operator>>(std::istream& in, UllLitIO&& dest)
 {
-  std::istream::sentry sentry(in);
-  if (!sentry)
-    return in;
   unsigned long long val;
-  if (!(in >> val))
-    return in;
-  if (!(in >> LabelIO{"ull"}))
-    return in;
-  dest.ref = val;
+  if (in >> val && in >> LabelIO{"ull"})
+    dest.ref = val;
+  else
+    in.setstate(std::ios::failbit);
   return in;
 }
 
 std::istream& operator>>(std::istream& in, RatLspIO&& dest)
 {
-  std::istream::sentry sentry(in);
-  if (!sentry)
-    return in;
   in >> DelimiterIO{'('} >> DelimiterIO{':'} >> LabelIO{"n"} >> dest.ref.first;
   in >> DelimiterIO{':'} >> LabelIO{"d"} >> dest.ref.second;
-  in >> DelimiterIO{')'};
+  in >> DelimiterIO{':'} >> DelimiterIO{')'};
   return in;
 }
 
 std::istream& operator>>(std::istream& in, StringIO&& dest)
 {
-  std::istream::sentry sentry(in);
-  if (!sentry)
-    return in;
   return std::getline(in >> DelimiterIO{'"'}, dest.ref, '"');
 }
 
@@ -104,40 +81,32 @@ std::istream& operator>>(std::istream& in, DataStruct& dest)
 
   DataStruct input;
   in >> DelimiterIO{'('} >> DelimiterIO{':'};
-
   for (int i = 0; i < 3; ++i) {
     std::string key;
     std::getline(in, key, ' ');
-    if (key == "key1") {
+    if (key == "key1")
       in >> UllLitIO{input.key1};
-    } else if (key == "key2") {
+    else if (key == "key2")
       in >> RatLspIO{input.key2};
-    } else if (key == "key3") {
+    else if (key == "key3")
       in >> StringIO{input.key3};
-    } else {
+    else {
       in.setstate(std::ios::failbit);
+      break;
     }
-
-    if (i < 2) {
+    if (i < 2)
       in >> DelimiterIO{':'};
-    }
   }
-
   in >> DelimiterIO{':'} >> DelimiterIO{')'};
-  if (in) {
+  if (in)
     dest = input;
-  }
   return in;
 }
 
 std::ostream& operator<<(std::ostream& out, const DataStruct& src)
 {
-  std::ostream::sentry sentry(out);
-  if (!sentry)
-    return out;
-  out << "(:key1 " << src.key1 << "ull";
-  out << ":key2 (:N " << src.key2.first << ":D " << src.key2.second << ":)";
-  out << ":key3 \"" << src.key3 << "\":)";
+  out << "(:key1 " << src.key1 << "ull:key2 (:N " << src.key2.first << ":D "
+      << src.key2.second << ":):key3 \"" << src.key3 << "\":)";
   return out;
 }
 
@@ -145,8 +114,10 @@ bool compareDataStruct(const DataStruct& a, const DataStruct& b)
 {
   if (a.key1 != b.key1)
     return a.key1 < b.key1;
-  if (a.key2 != b.key2)
-    return a.key2 < b.key2;
+  long long val1 = a.key2.first * static_cast< long long >(b.key2.second);
+  long long val2 = b.key2.first * static_cast< long long >(a.key2.second);
+  if (val1 != val2)
+    return val1 < val2;
   return a.key3.length() < b.key3.length();
 }
 }
@@ -156,20 +127,20 @@ int main()
   using namespace levkin;
   std::vector< DataStruct > data;
 
-  std::copy(
-      std::istream_iterator< DataStruct >(std::cin),
-      std::istream_iterator< DataStruct >(), std::back_inserter(data));
-
-  if (std::cin.fail() && !std::cin.eof()) {
-    std::cin.clear();
-    std::cin.ignore(std::numeric_limits< std::streamsize >::max(), '\n');
+  while (std::cin.peek() != EOF) {
+    DataStruct temp;
+    if (std::cin >> temp) {
+      data.push_back(temp);
+    } else {
+      std::cin.clear();
+      std::cin.ignore(std::numeric_limits< std::streamsize >::max(), '\n');
+    }
+    std::cin >> std::ws;
   }
 
   std::sort(data.begin(), data.end(), compareDataStruct);
-
   std::copy(
       data.begin(), data.end(),
       std::ostream_iterator< DataStruct >(std::cout, "\n"));
-
   return 0;
 }
