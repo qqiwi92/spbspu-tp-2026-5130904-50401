212c0c503e2771da564c0580b8fa952b6d6fafc7
Dmitry Levkin <mit.levkin@vk.com>
2026-06-18 16:53:22 +0300

    bad implementation input operator for p_t struct

--- levkin.dima/T2/main.cpp
+++ levkin.dima/T2/main.cpp
@@ -4,13 +4,13 @@
 #include <algorithm>
 
 namespace levkin {
-    struct p_t {
-        int x,y;
-    };
-    std::ostream & operator<<(std::ostream& os, p_t p);
-    std::istream & operator>>(std::istream& is, p_t p);
+struct p_t {
+  int x, y;
+};
+std::ostream& operator<<(std::ostream& os, p_t p);
+std::istream& operator>>(std::istream& is, p_t p);
 
-    bool operator<(p_t lhs, p_t rhs);
+bool operator<(p_t lhs, p_t rhs);
 }
 
 int main()
@@ -24,4 +24,40 @@ int main()
   std::sort(data.begin(), data.end());
 
   std::copy(data.begin(), data.end(), oit_t{std::cout, "\n"});
+}
+
+std::istream& levkin::operator>>(std::istream& is, p_t p);
+{
+  if (!is) {
+    return is;
+  }
+  char c = 0;
+  is >> c;
+  if (c != '(') {
+    is.setstate(std::ios_base::failbit);
+    return is;
+  }
+  int x = 0;
+  is >> x;
+  if (!is) {
+    return is;
+  }
+  is >> c;
+  if (c != ',') {
+  }
+  int y = 0;
+  is >> y;
+  if (!is) {
+    return is;
+  }
+
+  is >> c;
+  if (c != ')') {
+    is.setstate(std::ios_base::failbit);
+    return is;
+  }
+  if (is) {
+    p = p_t{x, y};
+  }
+  return is;
 }
