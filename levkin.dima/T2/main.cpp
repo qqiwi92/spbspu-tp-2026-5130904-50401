462f75499a6e4365f18b7103b05709df9ac18d94
Dmitry Levkin <mit.levkin@vk.com>
2026-06-18 22:20:52 +0300

    >> operators for all structs

--- levkin.dima/T2/main.cpp
+++ levkin.dima/T2/main.cpp
@@ -55,41 +55,89 @@ std::istream& operator>>(std::istream& in, DelimiterIO&& dest)
   return in;
 }
 
-std::istream& check(std::istream& is, char ch)
+std::istream& operator>>(std::istream& in, LabelIO&& dest)
 {
-  std::istream::sentry s(is);
-  if (s) {
-    if (is.peek() != ch) {
-      is.setstate(std::ios_base::failbit);
+  std::istream::sentry sentry(in);
+  if (!sentry)
+    return in;
+  for (char exp_c : dest.exp) {
+    char c = ' ';
+    in >> c;
+    if (in && std::tolower(c) != std::tolower(exp_c)) {
+      in.setstate(std::ios::failbit);
+      break;
+    }
+  }
+  return in;
+}
+
+std::istream& operator>>(std::istream& in, UllLitIO&& dest)
+{
+  std::istream::sentry sentry(in);
+  if (!sentry)
+    return in;
+  unsigned long long val;
+  if (!(in >> val))
+    return in;
+  if (!(in >> LabelIO{"ull"}))
+    return in;
+  dest.ref = val;
+  return in;
+}
+
+std::istream& operator>>(std::istream& in, RatLspIO&& dest)
+{
+  std::istream::sentry sentry(in);
+  if (!sentry)
+    return in;
+  in >> DelimiterIO{'('} >> DelimiterIO{':'} >> LabelIO{"n"} >> dest.ref.first;
+  in >> DelimiterIO{':'} >> LabelIO{"d"} >> dest.ref.second;
+  in >> DelimiterIO{')'};
+  return in;
+}
+
+std::istream& operator>>(std::istream& in, StringIO&& dest)
+{
+  std::istream::sentry sentry(in);
+  if (!sentry)
+    return in;
+  return std::getline(in >> DelimiterIO{'"'}, dest.ref, '"');
+}
+
+std::istream& operator>>(std::istream& in, DataStruct& dest)
+{
+  std::istream::sentry sentry(in);
+  if (!sentry)
+    return in;
+
+  DataStruct input;
+  in >> DelimiterIO{'('} >> DelimiterIO{':'};
+
+  for (int i = 0; i < 3; ++i) {
+    std::string key;
+    std::getline(in, key, ' ');
+    if (key == "key1") {
+      in >> UllLitIO{input.key1};
+    } else if (key == "key2") {
+      in >> RatLspIO{input.key2};
+    } else if (key == "key3") {
+      in >> StringIO{input.key3};
     } else {
-      is.get();
+      in.setstate(std::ios::failbit);
+    }
+
+    if (i < 2) {
+      in >> DelimiterIO{':'};
     }
   }
   return is;
 }
 
-std::istream& checkIgnoreCase(std::istream& is, char ch)
-{
-  std::istream::sentry s(is);
-  if (s) {
-    auto lowerC = std::tolower(static_cast< unsigned char >(is.peek()));
-    auto lowerCh = std::tolower(static_cast< unsigned char >(ch));
-
-    if (lowerC != lowerCh) {
-      is.setstate(std::ios_base::failbit);
-    } else {
-      is.get();
-    }
+  in >> DelimiterIO{':'} >> DelimiterIO{')'};
+  if (in) {
+    dest = input;
   }
-  return is;
-}
-
-std::istream& checkSpan(std::istream& is, std::string& str)
-{
-  for (char c : str) {
-    checkIgnoreCase(is, c);
-  }
-  return is;
+  return in;
 }
 
 std::istream& levkin::operator>>(std::istream& is, delimeter_t del)
