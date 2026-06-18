5713eb469c623b8e7ff745523219638e6d834ef8
Dmitry Levkin <mit.levkin@vk.com>
2026-06-18 17:05:53 +0300

    IO with pt works now

--- levkin.dima/T2/main.cpp
+++ levkin.dima/T2/main.cpp
@@ -8,17 +8,18 @@ struct p_t {
   int x, y;
 };
 std::ostream& operator<<(std::ostream& os, p_t p);
-std::istream& operator>>(std::istream& is, p_t p);
+std::istream& operator>>(std::istream& is, p_t& p);
 
 bool operator<(p_t lhs, p_t rhs);
 }
 
 int main()
 {
-  std::vector< int > data;
+  using T = levkin::p_t;
+  std::vector< T > data;
 
-  using iit_t = std::istream_iterator< int >;
-  using oit_t = std::ostream_iterator< int >;
+  using iit_t = std::istream_iterator< T >;
+  using oit_t = std::ostream_iterator< T >;
   std::copy(iit_t{std::cin}, iit_t{}, std::back_inserter(data));
 
   std::sort(data.begin(), data.end());
@@ -26,7 +27,12 @@ int main()
   std::copy(data.begin(), data.end(), oit_t{std::cout, "\n"});
 }
 
-std::istream& levkin::operator>>(std::istream& is, p_t p);
+std::ostream& levkin::operator<<(std::ostream& os, levkin::p_t p)
+{
+  return os << '(' << p.x << ',' << ' ' << p.y << ')';
+}
+
+std::istream& levkin::operator>>(std::istream& is, p_t& p)
 {
   if (!is) {
     return is;
@@ -44,7 +50,9 @@ std::istream& levkin::operator>>(std::istream& is, p_t p);
   }
   is >> c;
   if (c != ',') {
-  }
+     is.setstate(std::ios_base::failbit);
+     return is;
+   }
   int y = 0;
   is >> y;
   if (!is) {
@@ -60,4 +68,16 @@ std::istream& levkin::operator>>(std::istream& is, p_t p);
     p = p_t{x, y};
   }
   return is;
+}
+
+bool levkin::operator<(p_t lhs, p_t rhs)
+{
+  if (lhs.x < rhs.x) {
+    return true;
+  } else if (lhs.x == rhs.x) {
+    if (lhs.y < rhs.y) {
+      return true;
+    }
+  }
+  return false;
 }
