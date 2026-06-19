5b5515f099add5fc6205da7511344af89c7c503c
Dmitry Levkin <mit.levkin@vk.com>
2026-06-19 22:53:25 +0300

    Implement area, max, and min commands

--- levkin.dima/T3/commands.cpp
+++ levkin.dima/T3/commands.cpp
@@ -1,6 +1,6 @@
 #include "commands.hpp"
 #include <iostream>
-#include <memory>
+#include <iomanip>
 #include <string>
 #include <unordered_map>
 #include <vector>
@@ -20,18 +20,93 @@ std::string getWord(std::istream& in)
   return s;
 }
 
-double areaOfPolygon(Polygon & p) {
-    
-}
-
-void cmdArea(std::istream& in, std::ostream&, Database& db)
+void cmdArea(
+    std::istream& in, std::ostream& out, const std::vector< Polygon >& db)
 {
   std::string word = getWord(in);
+  if (word == "MEAN" && db.empty()) {
+    out << "<INVALID COMMAND>\n";
+    return;
+  }
+
+  std::vector< Polygon > filtered;
+  filtered.reserve(db.size());
+
+  using namespace detail;
   if (word == "EVEN") {
-      
+    std::copy_if(
+        db.begin(), db.end(), std::back_inserter(filtered),
+        std::bind(isEven, std::bind(getPolygonSize, std::placeholders::_1)));
+  } else if (word == "ODD") {
+    std::copy_if(
+        db.begin(), db.end(), std::back_inserter(filtered),
+        std::bind(isOdd, std::bind(getPolygonSize, std::placeholders::_1)));
+  } else if (word == "MEAN") {
+    std::copy(db.begin(), db.end(), std::back_inserter(filtered));
+  } else {
+    try {
+      size_t targetSize = std::stoull(word);
+      std::copy_if(
+          db.begin(), db.end(), std::back_inserter(filtered),
+          std::bind(
+              isSizeEqual, std::bind(getPolygonSize, std::placeholders::_1),
+              targetSize));
+    } catch (...) {
+      out << "<INVALID COMMAND>\n";
+      return;
+    }
+  }
+
+  double totalArea = polygonVectorAreaSum(filtered);
+
+  detail::IOguard guard(out);
+  out << std::fixed << std::setprecision(1);
+
+  if (word == "MEAN") {
+    out << (totalArea / db.size()) << "\n";
+  } else {
+    out << totalArea << "\n";
+  }
+}
+void cmdMax(
+    std::istream& in, std::ostream& out, const std::vector< Polygon >& db)
+{
+  std::string arg = getWord(in);
+  if (db.empty() || (arg != "AREA" && arg != "VERTEXES")) {
+    out << "<INVALID COMMAND>\n";
+    return;
+  }
+
+  detail::IOguard guard(out);
+  using namespace detail;
+  if (arg == "AREA") {
+    auto it = std::max_element(db.begin(), db.end(), compareAreaLess);
+    out << std::fixed << std::setprecision(1) << areaOfPolygon(*it) << "\n";
+  } else if (arg == "VERTEXES") {
+    auto it = std::max_element(db.begin(), db.end(), compareVertexesLess);
+    out << detail::getPolygonSize(*it) << "\n";
   }
 }
 
+void cmdMin(
+    std::istream& in, std::ostream& out, const std::vector< Polygon >& db)
+{
+  std::string arg = getWord(in);
+  if (db.empty() || (arg != "AREA" && arg != "VERTEXES")) {
+    out << "<INVALID COMMAND>\n";
+    return;
+  }
+
+  detail::IOguard guard(out);
+  using namespace detail;
+  if (arg == "AREA") {
+    auto it = std::min_element(db.begin(), db.end(), compareAreaLess);
+    out << std::fixed << std::setprecision(1) << areaOfPolygon(*it) << "\n";
+  } else if (arg == "VERTEXES") {
+    auto it = std::min_element(db.begin(), db.end(), compareVertexesLess);
+    out << detail::getPolygonSize(*it) << "\n";
+  }
+}
 Cmds getCmds()
 {
   Cmds cmds;
